3b316d9bc035ce85c050a71fa891b1b02046daa6
nick black <dankamongmen@gmail.com>
2019-06-05 22:22:34 -0400

    main: Parse --scansdo command line param

--- src/canscan/node.py
+++ src/canscan/node.py
@@ -1,8 +1,16 @@
 import canopen
 
+OD_IDENTITY = 0x1018
+OD_IDENTITY_VENDORID = 0x1
+
 class Node(object):
     """CANopen node bound to some device"""
 
     def __init__(self, dev):
         network = canopen.Network()
         network.connect(bustype='socketcan', channel=dev)
+
+    def CheckNodeSDO(self, nodeid):
+        """Hit some node (7-bit Node ID) with a quick Vendor Identity SDO."""
+        node = canopen.RemoteNode(6)
+        vendor_id = node.sdo[OD_IDENTITY][OD_IDENTITY_VENDORID].raw
