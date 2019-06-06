86f2c2b8a8eb656476b2ed175dae1dbb0a679b4a
nick black <dankamongmen@gmail.com>
2019-06-05 22:28:07 -0400

    main: handle empty scansdo list

--- src/canscan/__main__.py
+++ src/canscan/__main__.py
@@ -4,6 +4,7 @@
 #
 # https://github.com/dankamongmen/canscan
 
+from __future__ import print_function
 import node
 import argparse
 
@@ -14,26 +15,26 @@ if __name__ == '__main__':
                         type=lambda x: x.split(','))
     args = parser.parse_args()
     node.Node(args.device)
-    print(args.scansdo)
     scanNodes = []
     scanSet = set(scanNodes)
-    for noderange in args.scansdo:
-        try:
-            node = int(noderange)
-            if node < 0 or node > 0x7f:
-                raise SystemExit('Invalid node ID: %d' % node)
-            scanSet.add(node)
-        except ValueError:
-            rlist = ([int(n) for n in noderange.split('-')])
-            if len(rlist) != 2:
-                raise SystemExit('Invalid node ID range: %s' % noderange)
-            if rlist[0] < 0 or rlist[1] < 0:
-                raise SystemExit('Invalid node ID range: %s' % noderange)
-            if rlist[0] > 0x7f or rlist[1] > 0x7f:
-                raise SystemExit('Invalid node ID range: %s' % noderange)
-            if rlist[0] > rlist[1]:
-                raise SystemExit('Invalid node ID range: %s' % noderange)
-            r = set(range(rlist[0], rlist[1] + 1))
-            scanSet.update(r)
-    scanNodes = list(scanSet)
-    print(scanNodes)
+    if args.scansdo:
+        for noderange in args.scansdo:
+            try:
+                node = int(noderange)
+                if node < 0 or node > 0x7f:
+                    raise SystemExit('Invalid node ID: %d' % node)
+                scanSet.add(node)
+            except ValueError:
+                rlist = ([int(n) for n in noderange.split('-')])
+                if len(rlist) != 2:
+                    raise SystemExit('Invalid node ID range: %s' % noderange)
+                if rlist[0] < 0 or rlist[1] < 0:
+                    raise SystemExit('Invalid node ID range: %s' % noderange)
+                if rlist[0] > 0x7f or rlist[1] > 0x7f:
+                    raise SystemExit('Invalid node ID range: %s' % noderange)
+                if rlist[0] > rlist[1]:
+                    raise SystemExit('Invalid node ID range: %s' % noderange)
+                r = set(range(rlist[0], rlist[1] + 1))
+                scanSet.update(r)
+        scanNodes = list(scanSet)
+    print('Node scan list:', scanNodes)
