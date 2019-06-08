a58a8e1f986b1a2e5d3e5dced73495881be0b2ac
nick black <dankamongmen@gmail.com>
2019-06-08 04:43:59 -0400

    [node] correct destructor check for network

--- src/canscan/node.py
+++ src/canscan/node.py
@@ -6,21 +6,14 @@ OD_IDENTITY_VENDORID = 0x1
 class Node(object):
     """CANopen node bound to some device"""
 
-    def Connectable(self):
-        """Returns true if we've got, or can make, a CAN connection."""
-        if self.dev:
-            return True
-        return False
-
     def __init__(self, bustype='virtual', dev=''):
         """An empty dev param means we won't try to actually connect."""
         self.dev = dev
-        if self.Connectable():
-            self.network = canopen.Network()
-            self.network.connect(bustype=bustype, channel=dev)
+        self.network = canopen.Network()
+        self.network.connect(bustype=bustype, channel=dev)
 
     def __del__(self):
-        if self.Connectable():
+        if self.network:
             print("Disconnecting from CAN...")
             self.network.disconnect()
 
